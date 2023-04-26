89c6daea611a9a00af66db644ab421cc14c0d8c8
Jameer <jimcute8879@gmail.com>
2023-04-26 14:44:51 +0530

    Removed Unnecessary Files.

--- myblog-api/src/app.module.ts
+++ myblog-api/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DbModule } from './utils/db.module';
 import { BlogModule } from './blog/blog.module';
 import { ConfigurationModule } from './config/config.module';
 
 @Module({
-  imports: [ConfigurationModule, DbModule, BlogModule],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [ConfigurationModule, DbModule, BlogModule]
 })
 export class AppModule {}
