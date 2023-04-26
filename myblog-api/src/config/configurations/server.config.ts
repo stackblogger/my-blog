cb16b15bcce8fec98cbb4421b91b34233551e922
Jameer <jimcute8879@gmail.com>
2023-04-26 14:43:49 +0530

    Dynamic Host and Port Setup

--- myblog-api/src/config/configurations/server.config.ts
+++ myblog-api/src/config/configurations/server.config.ts
@@ -1,10 +1,14 @@
 export interface ServerConfig {
   database: string;
+  host: string;
+  port: number;
 }
 
 function getServerConfig(): ServerConfig {
   return {
-    database: process.env.DATABASE_URI
+    database: process.env.DATABASE_URI,
+    host: process.env.HOST,
+    port: parseInt(process.env.PORT || '8888', 10)
   };
 }
 
--- myblog-api/src/main.ts
+++ myblog-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import getServerConfig from './config/configurations/server.config';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(getServerConfig().port, getServerConfig().host);
+  console.log(`Server is listening at port ${getServerConfig().port} and host ${getServerConfig().host}`);
 }
 bootstrap();
