9b48021dd3f3211feb337f05fd36d84743fbc36e
Jameer <jimcute8879@gmail.com>
2023-04-27 15:51:42 +0530

    Enable CORS.

--- myblog-api/src/main.ts
+++ myblog-api/src/main.ts
@@ -4,6 +4,7 @@ import getServerConfig from './config/configurations/server.config';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   await app.listen(getServerConfig().port, getServerConfig().host);
   console.log(`Server is listening at port ${getServerConfig().port} and host ${getServerConfig().host}`);
