198eec0058d8b0354c5bbb27ddce0ed1bc279f15
Jameer <jimcute8879@gmail.com>
2023-04-27 12:37:54 +0530

    Helper Function to Convert String to Slug

---
+++ myblog-api/src/blog/utils/helper.ts
@@ -0,0 +1,11 @@
+export function ConvertToSlug(str: string): string {
+  return str
+    .replace(/\s/g, '-') // replace all spaces with -
+    .toLowerCase()
+    .replace(/[^\w\s]/g, '-') // replace all special chars with -
+    .replace(/[0-9]/g, '') // remove all numbers
+    .replace(/\-{2,}/g, '-') // replace more than one - with single -
+    .replace(/-$/, '') // replace last char if its -
+    .trim() // remove any spaces from start or end
+    .replace(/^-/, ''); // remove first - if there is any
+}
