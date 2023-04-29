a43e0793b2d1a3bc37eeb6f0ca66090c06b4fd09
Jameer <jimcute8879@gmail.com>
2023-04-29 17:18:01 +0530

    Setting Up Deployments

--- myblog-api/src/app.module.ts
+++ myblog-api/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { BlogModule } from './blog/blog.module';
 import { ConfigurationModule } from './config/config.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigurationModule, BlogModule]
+  imports: [
+    ConfigurationModule,
+    BlogModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'myblog-ui')
+    })
+  ]
 })
 export class AppModule {}
