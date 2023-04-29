7517129da920d2f48cbe029eb731b70c724c2626
Jameer <jimcute8879@gmail.com>
2023-04-29 17:17:36 +0530

    E2E Test Cases

--- myblog-api/test/app.e2e-spec.ts
+++ myblog-api/test/app.e2e-spec.ts
@@ -3,6 +3,9 @@ import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+const accessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImppbWN1dGU4ODc5QGdtYWlsLmNvbSIsInN1YiI6IjY0NDk3YmRjMWU1NmY2Zjc0ZGRjMmY2NiIsImlhdCI6MTY4MjYzMTY2MSwiZXhwIjoxNjg1MjIzNjYxfQ.N-uoFHoq3SPAXI0mzopKltY0jjETGQEVoMyhWzOg8Kg';
+
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -15,7 +18,41 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer()).get('/').expect(200).expect('Hello World!');
+  it('/blogs (GET) without access token', () => {
+    return request(app.getHttpServer()).get('/blogs').expect(401);
+  });
+
+  it('/blogs (POST) with access token', () => {
+    return request(app.getHttpServer())
+      .post('/blogs')
+      .set('Authorization', 'Bearer ' + accessToken)
+      .send({
+        title: 'Some Article Title Here',
+        body: '<p>Some Content Body</p>',
+        tags: [
+          {
+            name: 'programming'
+          },
+          {
+            name: 'javascript'
+          }
+        ],
+        category: {
+          name: 'Angular'
+        }
+      })
+      .expect(201);
+  });
+
+  it('/blogs (GET) with access token', () => {
+    return request(app.getHttpServer())
+      .get('/blogs')
+      .query({ pageSize: 10, currentPage: 1 })
+      .set('Authorization', 'Bearer ' + accessToken)
+      .expect(200);
+  });
+
+  it('/blogs (POST) without access token', () => {
+    return request(app.getHttpServer()).post('/blogs').expect(401);
   });
 });
